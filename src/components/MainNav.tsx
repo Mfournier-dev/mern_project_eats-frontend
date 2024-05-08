24ae68d4dc88bdf3d72adfbd01558dae2a894f60
Michel Fournier <m.fournier.a@gmail.com>
2024-05-08 12:51:46 -0400

    view order features

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,6 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 import UsernameMenu from "./UsernameMenu";
+import { Link } from "react-router-dom";
 
 const MainNav = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
@@ -8,7 +9,16 @@ const MainNav = () => {
     <>
       <span className="flex space-x-2 items-center">
         {isAuthenticated ? (
-          <UsernameMenu />
+          <>
+            <Link
+              to="/order-status"
+              className="font-bold hover:text-orange-500"
+            >
+              Order Status
+            </Link>
+
+            <UsernameMenu />
+          </>
         ) : (
           <Button
             variant="ghost"
@@ -19,9 +29,8 @@ const MainNav = () => {
           </Button>
         )}
       </span>
-
     </>
   );
 };
 
-export default MainNav
+export default MainNav;
